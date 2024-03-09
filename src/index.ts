23d6d2fd0079b9ba24c6bce571616964f4bd6ed7
denis.tsarik <denis.tsarik@stark.games>
2024-03-09 23:34:31 +0300

    fix: start server

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,14 @@ import express from 'express'
 
 config()
 const app = express()
+const port = 3000;
+app.get('/',(req,res)=>{
+    res.send('Test log')
+})
+
+app.listen(port,()=>{
+    console.log(`Example app listening on port ${port}`)
+})
 
 // const TELEGRAM_URI = `https://api.telegram.org/bot${process.env.TELEGRAM_API_TOKEN}/sendMessage`
 
