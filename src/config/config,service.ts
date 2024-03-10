e7b788564e76b0bae079cb030a09b30ead0b7811
denis.tsarik <denis.tsarik@stark.games>
2024-03-10 13:11:17 +0300

    test commit

--- src/config/config,service.ts
+++ src/config/config,service.ts
@@ -2,26 +2,32 @@ import { DotenvParseOutput, config } from 'dotenv';
 import { IConfigService } from './config.interface';
 
 export class ConfigService implements IConfigService {
-  private readonly config: DotenvParseOutput;
+  private readonly config: DotenvParseOutput | undefined;
 
   constructor() {
     const { error, parsed } = config();
-    if (error) {
-      throw new Error('.env is undefined');
-    }
-    if (!parsed) {
-      throw new Error('.env is empty');
-    }
+    // if (error) {
+    //   throw new Error('.env is undefined');
+    // }
+    // if (!parsed) {
+    //   throw new Error('.env is empty');
+    // }
 
-    this.config = parsed;
+    if (!error && parsed) {
+      this.config = parsed;
+    }
   }
 
   get(key: string): string {
-    const res = this.config[key];
-    if (!res) {
-      throw new Error('no such key');
-    }
+    if (process.env.NODE_ENV !== 'production' && this.config) {
+      const res = this.config[key];
+      if (!res) {
+        throw new Error('no such key');
+      }
 
-    return res;
+      return res;
+    } else {
+      return process.env.TELEGRAM_API_TOKEN || "";
+    }
   }
 }
